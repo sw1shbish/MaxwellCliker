a887d8f8ca9f16ababe06145bfa7dcd79292961d
user <usr123@gmail.com>
2024-06-11 09:49:51 +0300

    cliker

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    int counter = 0;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+
+    private void Btn_OnClick(object sender, RoutedEventArgs e)
+    {
+        counter++;
+        Counter.Content =  "MaxwellCounts: " + counter;
+    }
+}
